748300cf7889ed06e73d27317cdfeab359cb0011
Aliaksandra Khvaliuk <a.hvaluk@gmail.com>
2022-02-21 11:06:43 +0100

    React components Part 1 Menu with Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
 import './App.css';
 import React, {Component} from "react";
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+         <Menu />
       </div>
     );
   }
