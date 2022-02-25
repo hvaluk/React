887141f01efdd3cdfcb4faa9bf1351a25cbb1de7
Aliaksandra Khvaliuk <a.hvaluk@gmail.com>
2022-02-25 14:36:16 +0100

    extend card detail component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent'
 
 
 class Menu extends Component {
@@ -14,23 +15,6 @@ class Menu extends Component {
 
     onDishSelect(dish) {
         this.setState({ selectedDish: dish});
-    }
-
-    renderDish(dish) {
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return(
-                <div></div>
-            );
     }
 
     render() {
@@ -55,8 +39,8 @@ class Menu extends Component {
                 </div>
             <div className="row">
               
-                  <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
+                    <div className="col-12 col-md-5 m-1">
+                        <DishDetail dish={this.state.selectedDish} />
                   </div>
                 </div>
             </div>
