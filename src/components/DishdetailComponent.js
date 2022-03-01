8d9ec21bb1da76ad814f37d37e5e159f4e2c1289
Aliaksandra Khvaliuk <a.hvaluk@gmail.com>
2022-03-01 14:15:52 +0100

    Assignment: React Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,19 +5,44 @@ class DishDetail extends Component {
 
     render() {
         if (this.props.dish != null)
-            return(
-                <Card>
-                    <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-                    <CardBody>
-                      <CardTitle>{this.props.dish.name}</CardTitle>
-                      <CardText>{this.props.dish.description}</CardText>
-                    </CardBody>
-                </Card>
+            return ( 
+                <div className='row'>
+                    <div className='col-12 col-md-5 m-1'>
+                       <Card>
+                            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                            <CardBody>
+                              <CardTitle>{this.props.dish.name}</CardTitle>
+                              <CardText>{this.props.dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className='col-12 col-md-5 m-1'> 
+                        {this.renderComments()}
+                    </div>
+               </div>
             );
         else
             return(
                 <div></div>
             );
+    }
+    renderComments() {
+        return (
+            <div>
+                <h4>Comment</h4>
+                <ul className='list-unstyled'>
+                    {this.props.dish.comments.map((c) => {
+                        return (
+                            <li>
+                                <div>{c.comment}</div>
+                                <div>--{c.author}, {c.date}</div>
+                            </li>
+                        )
+                    })
+                    }                    
+                </ul>
+            </div>  
+        )
     }
 }
 
